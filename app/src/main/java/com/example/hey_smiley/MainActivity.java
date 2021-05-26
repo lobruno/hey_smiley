b4b19414e0e676604474fb5a4aad1593205c4259
lobruno <daniarbeknurbol@gmail.com>
2021-05-26 11:39:10 +0300

    added a check for mail confirmation

--- app/src/main/java/com/example/hey_smiley/MainActivity.java
+++ app/src/main/java/com/example/hey_smiley/MainActivity.java
@@ -164,19 +164,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-//    String anekdot() throws Exception {
-//        String sURL = "http://rzhunemogu.ru/RandJSON.aspx?CType=1"; //just a string
-//
-//        URL url = new URL(sURL);
-//        URLConnection request = url.openConnection();
-//        request.connect();
-//        JsonParser jp = new JsonParser(); //from gson
-//        JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent())); //Convert the input stream to a json element
-//        JsonObject rootobj = root.getAsJsonObject(); //May be an array, may be an object.
-//        String anek = rootobj.getAsString();
-//        return  anek;
-//        //String zipcode = rootobj.get("zip_code").getAsString(); //just grab the zipcode
-//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
